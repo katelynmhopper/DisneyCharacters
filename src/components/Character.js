02fd4eb8a374c8cbe8d73146e231526d131a162e
katelynmhopper <katelynmhopper@gmail.com>
2022-06-03 11:58:01 -0600

    update

---
+++ src/components/Character.js
@@ -0,0 +1,14 @@
+import React from 'react';
+export const Character = ({ name, films, tvShows }) => {
+    console.log( name, films, tvShows)
+    return ( <>
+        <h3>{name}</h3> 
+        <div className="character">
+        <ul>
+            <li> films: {films} </li>
+            <li> tv shows: {tvShows} </li>
+        </ul>
+        </div>
+        </>
+    )
+}
